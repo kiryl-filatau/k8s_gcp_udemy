d0b6c29067eb4f2c634ebffbdfcacaf212c31748
Kiryl Filatau <kiryl_filatau@epam.com>
2019-08-20 22:38:35 +0300

    added deployment scripts typo fix

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ docker build -t filatko/multi-server:latest -t filatko/multi-server:$SHA -f ./se
 docker build -t filatko/multi-worker:latest -t filatko/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push filatko/multi-client:latest
 docker push filatko/multi-server:latest
-docker push filatko/mulri-worker:latest
+docker push filatko/multi-worker:latest
 docker push filatko/multi-client:$SHA
 docker push filatko/multi-server:$SHA
-docker push filatko/mulri-worker:$SHA
+docker push filatko/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=filatko/multi-server:$SHA
 kubectl set image deployments/client-deployment client=filatko/multi-client:$SHA
